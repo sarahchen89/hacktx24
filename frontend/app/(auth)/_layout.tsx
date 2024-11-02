a4976f9a9e1260879124c09ec72a7bfe9dd8e713
Raghu Talla <rtalla007@gmail.com>
2024-11-02 15:22:31 -0500

    structure frontend pages and auth

---
+++ frontend/app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function RootLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="signup" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    );
+  }
